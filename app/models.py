343e6a8115ee5af8a2cc93d8cf7b238d4b2d4bc6
bachirou2025 <mohamadoubachirouhouseini3946@gmail.com>
2026-02-10 15:18:24 +0100

    feat: implement POST /projects endpoint(Issue #1)

---
+++ app/models.py
@@ -0,0 +1,12 @@
+
+from pydantic import BaseModel
+from typing import Optional
+
+
+class Project(BaseModel):
+    id: int
+    title: str
+    student_name: str
+    course: str
+    description: Optional[str] = None
+    grade: Optional[float] = None
---
+++ app/utils/db_manager.py
@@ -0,0 +1,18 @@
+
+import json
+
+DB_FILE = "db.json"
+
+
+def read_db():
+    try:
+        with open(DB_FILE, "r") as file:
+            # Json.load Transforme le json en dictionnaire python
+            return json.load(file)
+    except FileNotFoundError:
+        return {"projects": []}
+
+
+def write_db(data):
+    with open(DB_FILE, "w") as file:
+        json.dump(data, file, indent=4)
