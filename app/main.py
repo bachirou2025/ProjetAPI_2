144045f1c1795ffaad4b283341159a6638504e63
bachirou2025 <mohamadoubachirouhouseini3946@gmail.com>
2026-02-10 10:31:56 +0100

    Initial commit: structure FastAPI initiale

---
+++ app/main.py
@@ -0,0 +1,15 @@
+
+from fastapi import FastAPI
+from app.routers import projects
+
+app = FastAPI(
+    title="ProjetAPI",
+    description="API de gestion des projets étudiants",
+    version="1.0.0"
+)
+
+@app.get("/")
+def read_root():
+    return {"message": "Bienvenue dans ProjetAPI"}
+
+app.include_router(projects.router)
---
+++ app/routers/projects.py
@@ -0,0 +1,8 @@
+
+from fastapi import APIRouter
+
+router = APIRouter(prefix="/projects", tags=["Projects"])
+
+@router.get("/")
+def get_projects():
+    return {"projects": []}
