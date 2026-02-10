826d8f1135d610e65f444eff2c627a61c3cc6ac8
bachirou2025 <mohamadoubachirouhouseini3946@gmail.com>
2026-02-10 21:19:39 +0100

    test: hook pre-commit

--- app/main.py
+++ app/main.py
@@ -1,15 +1,18 @@
-
 from fastapi import FastAPI
 from app.routers import projects
 
 app = FastAPI(
     title="ProjetAPI",
     description="API de gestion des projets étudiants",
-    version="1.0.0"
+    version="1.0.0",
 )
+
 
 @app.get("/")
 def read_root():
     return {"message": "Bienvenue dans ProjetAPI"}
+
+
+x = 1
 
 app.include_router(projects.router)
